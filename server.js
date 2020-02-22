1bf0c4b9b104ebb28ca2bb73a244b865d612abec
gggbb <wangzifeng2008@qq.com>
2020-02-22 19:13:27 +0800

    ch users.id

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let http = require('http')
 let users = [
-  {id: 11, name: 'zifeng1'},
-  {id: 22, name: 'zifeng2'},
-  {id: 33, name: 'zifeng3'}
+  {id: 111, name: 'zifeng1'},
+  {id: 222, name: 'zifeng2'},
+  {id: 333, name: 'zifeng3'}
 ]
 let server = http.createServer(function(req, res){
   res.setHeader('Access-Control-Allow-Origin', '*')
